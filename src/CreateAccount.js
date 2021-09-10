a08a1018e2a068ed7e527dbb12fbe4466a52e0ea
Aytac <51949744+aytacismayil@users.noreply.github.com>
2021-09-10 09:42:14 +0400

    Update CreateAccount.js

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 
 import "./CreateAccount.css";
 
@@ -6,17 +6,16 @@ const emailRegex = RegExp(
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 );
 
-
 const formValid = ({ formErrors, ...rest }) => {
   let valid = true;
 
   // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
+  Object.values(formErrors).forEach((val) => {
     val.length > 0 && (valid = false);
   });
 
   // validate the form was filled out
-  Object.values(rest).forEach(val => {
+  Object.values(rest).forEach((val) => {
     val === null && (valid = false);
   });
 
@@ -28,26 +27,20 @@ class CreateAccount extends Component {
     super(props);
 
     this.state = {
-      firstName: null,    
+      firstName: null,
       email: null,
       password: null,
       describe: null,
       hidden: true,
-      password: '',
+      password: "",
       formErrors: {
-      firstName: "",        
-      email: "",
-      password: ""
+        firstName: "",
+        email: "",
+        password: "",
       },
-      isLocked: false
+      isLocked: false,
     };
-
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.toggleShow = this.toggleShow.bind(this);
-  }
-
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
   }
   toggleShow() {
     this.setState({ hidden: !this.state.hidden });
@@ -58,144 +51,72 @@ class CreateAccount extends Component {
     }
   }
 
-  checkform(){
-    if(
-      this.state.firstName !== null && 
+  checkform() {
+    if (
+      this.state.firstName !== null &&
+      this.state.firstName.length >= 5 &&
       this.state.email !== null &&
-      emailRegex.test(this.state.email)
-      && this.state.password !== null &&
-      this.state.password.length >= 8)
-      {
-        this.setState({isLocked: true});
-        console.log(this.state)
-      };
+      emailRegex.test(this.state.email) &&
+      this.state.password !== null &&
+      this.state.password.length >= 8
+    ) {
+      this.setState({ isLocked: true });
+    }
   }
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
-
-    if (formValid(this.state)) {
-      
-      console.log(`
-        --SUBMITTING--
-        First Name: ${this.state.firstName}       
-        Email: ${this.state.email}
-        Password: ${this.state.password}
-      `);
-    } else {
-      console.error("DISPLAY ERROR MESSAGE");
-    }
   };
 
-  handleChange = e => {
-    e.preventDefault();
+  handleChange = (e) => {
     const { name, value } = e.target;
     let formErrors = { ...this.state.formErrors };
     switch (name) {
       case "firstName":
-        formErrors.firstName =
-          value.length < 5 ? "Minimum 5 characters" : "";
-          this.setState({isLocked: false});
+        formErrors.firstName = value.length < 5 ? "Minimum 5 characters" : "";
         break;
-     
+
       case "email":
         formErrors.email = emailRegex.test(value)
           ? ""
           : "Please enter a valid email address";
-          this.setState({isLocked: false});
         break;
       case "password":
-        formErrors.password =
-          value.length < 8 ? "Minimum 8 characters" : "";
-          this.setState({isLocked: false});
+        formErrors.password = value.length < 8 ? "Minimum 8 characters" : "";
         break;
       case "describe":
-        formErrors.describe = 
-          value === "none" ? "Select": "";
-          this.setState({isLocked: false});
-        break;
-      case "":
-        this.setState({isLocked: true});
+        formErrors.describe = value === "none" ? "Select" : "";
         break;
     }
+    this.setState({ isLocked: false }, () => this.checkform());
+    this.setState(
+      { formErrors, [name]: value },
+      () => this.state,
+      () => this.checkform()
+    );
     
-    console.log('Value : ' + value);
-
-    this.checkform();
-
-    this.setState({ formErrors, [name]: value }, () => this.state);
-    // console.log(value, ':', this.state)
-    // if (this.state.firstName === null || this.state.firstName.match(/^ *$/) !== null){
-    //   this.setState({isLocked: false});
-    //   console.log("firstName");
-    // }
-    // else if (this.state.email === null || this.state.email.match(/^ *$/) !== null || !emailRegex.test(this.state.email)){
-    //   this.setState({isLocked: false});
-    //   console.log("email");
-    // }
-    // else if (this.state.password === null || this.state.password.match(/^ *$/) !== null || this.state.password.length < 8){ 
-    //   this.setState({isLocked: false});
-    //   console.log("Password");
-    // }
-    // else{
-    //   this.setState({isLocked: true});
-    //   console.log("Son");
-    //   console.log(this.state);
-    // };
-    // // this.checkform();
-    // this.setState(this.state.value);
-
-
-    // if (this.state.firstName === null ){
-    //   this.setState({isLocked: false});
-    //   console.log("firstName");
-    // }
-    // else if (this.state.email === null || !emailRegex.test(value)){
-    //   this.setState({isLocked: false});
-    //   console.log("email");
-    // }
-    // else if (this.state.password === null){ 
-    //   this.setState({isLocked: false});
-    //   console.log("Password");
-    // }
-    // else if (value === null || value === "none"){
-    //   this.setState({isLocked: false});
-    //   console.log("Value");
-    // }
-    // else{
-    //   this.setState({isLocked: true});
-    //   console.log("Son");
-    // };
-    // // this.checkform();
-    // this.setState(this.state.value);
   };
-
-
 
   render() {
     const { formErrors } = this.state;
     const lockedStyle = {
       backgroundColor: "#286EFA",
-      border: '1px solid #286EFA', 
+      border: "1px solid #286EFA",
       color: "#FFFFFF",
     };
 
     const unlockedStyle = {
       backgroundColor: "#EDEDED",
-      border: '1px solid #EDEDED',       
+      border: "1px solid #EDEDED",
       color: "#AAAAAA",
     };
-
     return (
-
       <div className="wrapper">
         <div className="form-wrapper">
-         
           <form onSubmit={this.handleSubmit} noValidate>
             <div className="input_group firstName">
-            
               <input
-                className= {formErrors.firstName.length > 0 ? "error" : null}
+                className={formErrors.firstName.length > 0 ? "error" : null}
                 // placeholder="First Name"
                 type="text"
                 name="firstName"
@@ -203,13 +124,17 @@ class CreateAccount extends Component {
                 required
                 onChange={this.handleChange}
               />
-                <label htmlFor="firstName" className={formErrors.firstName.length > 0 ? "error" : null}>Your name</label>
+              <label
+                htmlFor="firstName"
+                className={formErrors.firstName.length > 0 ? "error" : null}
+              >
+                Your name
+              </label>
               {formErrors.firstName.length > 0 && (
                 <span className="errorMessage">{formErrors.firstName}</span>
               )}
             </div>
             <div className="input_group email">
-             
               <input
                 className={formErrors.email.length > 0 ? "error" : null}
                 // placeholder="Email"
@@ -219,47 +144,63 @@ class CreateAccount extends Component {
                 required
                 onChange={this.handleChange}
               />
-               <label htmlFor="email" className={formErrors.email.length > 0 ? "error" : null}>Email address</label>
+              <label
+                htmlFor="email"
+                className={formErrors.email.length > 0 ? "error" : null}
+              >
+                Email address
+              </label>
               {formErrors.email.length > 0 && (
                 <span className="errorMessage">{formErrors.email}</span>
               )}
             </div>
 
-            <div className="firstName describe">          
-               <select name="describe" onChange={this.handleChange} id="selectBox">   
-                  <option value="none">I would describe my user type as</option>
-                  <option value="developer">Developer</option>
-                  <option value="design">Design</option>
-               </select>
-            </div>        
-           
+            <div className="firstName describe">
+              <select
+                name="describe"
+                onChange={this.handleChange}
+                id="selectBox"
+              >
+                <option value="none">I would describe my user type as</option>
+                <option value="developer">Developer</option>
+                <option value="design">Design</option>
+              </select>
+            </div>
+
             <div className="input_group password">
-           
               <input
                 className={formErrors.password.length > 0 ? "error" : null}
                 // placeholder="Password"
-                type={this.state.hidden ? 'password' : 'text'}
+                type={this.state.hidden ? "password" : "text"}
                 name="password"
                 noValidate
-               
                 onChange={this.handleChange}
               />
-              <label htmlFor="password" className={formErrors.password.length > 0 ? "error" : null}>Password</label>
+              <label
+                htmlFor="password"
+                className={formErrors.password.length > 0 ? "error" : null}
+              >
+                Password
+              </label>
               {formErrors.password.length > 0 && (
                 <span className="errorMessage">{formErrors.password}</span>
               )}
-              <button className="passwordBtn" onClick={this.toggleShow}><i className="fas fa-eye"></i></button>
+              <button className="passwordBtn" onClick={this.toggleShow}>
+                <i className="fas fa-eye"></i>
+              </button>
             </div>
             <div className="createAccount">
               {/* {console.log(this.state.isLocked)} */}
-              <button style={this.state.isLocked ? lockedStyle : unlockedStyle} type="submit">Next</button>
-             
+              <button
+                style={this.state.isLocked ? lockedStyle : unlockedStyle}
+                type="submit"
+              >
+                Next
+              </button>
             </div>
           </form>
         </div>
       </div>
-    
-     
     );
   }
 }
